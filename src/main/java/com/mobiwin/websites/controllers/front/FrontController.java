7fa160c2f070d3efd089c191e07d1ba39e44d052
J Pebs <juri.pebrianto@mobiwin.co.id>
2021-01-14 12:56:27 +0700

    Fragment
    Fragment

--- src/main/java/com/mobiwin/websites/controllers/front/FrontController.java
+++ src/main/java/com/mobiwin/websites/controllers/front/FrontController.java
@@ -1,6 +1,7 @@
 package com.mobiwin.websites.controllers.front;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -8,8 +9,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class FrontController {
     
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String Index() {
+    public String Index(Model model) {
 
+        model.addAttribute("content", "public/front/fragments/main");
         return "public/front/index";
     }
 }
